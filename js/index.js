25342655f5b976a9d9f89b31ad632ec80b68b0e7
rabah babaci <4rabah@gmail.com>
2020-01-16 17:04:55 -0800

    add animation to the header on mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+// add animation on mouseover event
+let navLinks = document.getElementsByClassName('nav-link');
+let header = document.body.firstElementChild;
+
+header.addEventListener('mouseover', (e) => {
+    if (header !== event.target) {
+        return;
+    }
+    e.target.classList.toggle('animation');
+    e.target.style.color = 'orange';
+});
+header.addEventListener('mouseleave', (e) => {
+    if (e.target !== header) {
+        return;
+    }
+    e.target.classList.toggle('animation');
+});
+
